116969e6b27126b33605b20833d668baa1c0253d
BlueTreehouse <jimmypark92@gmail.com>
2025-02-14 13:08:31 -0500

    3: added player draw

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -11,15 +12,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
+        # set's framerate
         dt = clock.tick(60) / 1000
+
+
+
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 
