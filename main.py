4e91f695cfa3b542207dbd9234d742a7bbb705ba
BlueTreehouse <jimmypark92@gmail.com>
2025-02-14 12:32:57 -0500

    2: added Frame limit

--- main.py
+++ main.py
@@ -2,19 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 
